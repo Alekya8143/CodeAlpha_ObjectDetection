f611d7297a513b5767ba40fd95f7d86242166d38
Alekya <alekyagolla16@gmail.com>
2026-03-04 15:43:49 +0530

    Added README and requirements

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 import time
 
-# Load model
+
 model = YOLO("yolov8n.pt")
 
 cap = cv2.VideoCapture(0)
@@ -14,20 +14,20 @@ while True:
     if not ret:
         break
 
-    # Detection + Tracking
+    
     results = model.track(frame, persist=True)
 
     annotated_frame = results[0].plot()
 
-    # Object count
+    
     count = len(results[0].boxes) if results[0].boxes is not None else 0
 
-    # FPS calculation
+    
     curr_time = time.time()
     fps = 1 / (curr_time - prev_time) if prev_time != 0 else 0
     prev_time = curr_time
 
-    # Display info
+    
     cv2.putText(annotated_frame, f"Objects: {count}", (20, 40),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
 
@@ -39,13 +39,13 @@ while True:
 
     cv2.imshow("Object Detection & Tracking", annotated_frame)
 
-    # Press S → save screenshot
+    
     key = cv2.waitKey(1)
     if key == ord("s"):
         cv2.imwrite("capture.jpg", annotated_frame)
         print("Screenshot saved!")
 
-    # Press Q → quit
+    
     if key == ord("q"):
         break
 
